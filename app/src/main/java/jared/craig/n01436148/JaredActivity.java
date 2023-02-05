7f204c9c3bc606cc3361c9084668754528172c65
Jared Craig <jcraig7152@gmail.com>
2023-02-04 20:45:27 -0500

    Initial commit

---
+++ app/src/main/java/jared/craig/n01436148/JaredActivity.java
@@ -0,0 +1,14 @@
+package jared.craig.n01436148;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JaredActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
